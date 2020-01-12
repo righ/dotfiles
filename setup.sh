e611721003a1b18a63fce6e201016023eb94185c
righ <django.hamster@gmail.com>
2020-01-12 18:17:55 +0900

    become

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 sudo timedatectl set-timezone Asia/Tokyo
 
 sudo apt-get install -y ansible
-sudo ansible-playbook install.yaml
-
+ansible-playbook install.yaml
 ansible-playbook setup.yaml
 ./deploy.sh
