2fac3602692570894b8446545acc4478892eb054
righ <django.hamster@gmail.com>
2020-01-12 22:15:27 +0900

    sh

---
+++ install.sh
@@ -0,0 +1,45 @@
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+ "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+ $(lsb_release -cs) \
+ stable"
+
+sudo apt-get update -y
+
+# builders
+sudo apt-get install -y \
+  make \
+  build-essential \
+  libssl-dev \
+  zlib1g-dev \
+  libbz2-dev \
+  libreadline-dev \
+  libsqlite3-dev \
+  libncurses5-dev \
+  libncursesw5-dev \
+  llvm \
+  xz-utils \
+  tk-dev
+
+# tools
+sudo apt-get install -y \
+  wget \
+  curl \
+  unzip \
+  git \
+  tmux \
+  apt-transport-https \
+  ca-certificates \
+  gnupg-agent \
+  software-properties-common \
+  mysql-client \
+  postgresql-client \
+  sqlite3 \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  direnv \
+  jq \
+
+
--- setup.sh
+++ setup.sh
@@ -1,8 +1,65 @@
 #!/usr/bin/env bash
 
-sudo timedatectl set-timezone Asia/Tokyo
+GO_VERSION=1.13.4
+PYTHON_VERSION=3.8.1
+RUBY_VERSION=2.7.0
+NODE_VERSION=13.6.0
+R_VERSION=3.6.2
+HASKELL_VERSION=8.6.3
+TERRAFORM_VERSION=0.12.19
 
-sudo apt-get install -y ansible
-ansible-playbook install.yaml
+
+sudo timedatectl set-timezone Asia/Tokyo
+./install.sh
 ./deploy.sh
-ansible-playbook setup.yaml
+
+git clone https://github.com/anyenv/anyenv ~/.anyenv
+expect -c "
+  spawn ~/.anyenv/bin/anyenv install --init
+  expect {
+    default { exit 0 }
+    \"y/N\" { send \"y\n\" }
+  }
+  interact
+"
+$(~/.anyenv/bin/anyenv init -)
+
+# goenv
+~/.anyenv/bin/anyenv install goenv
+goenv install $GO_VERSION
+goenv global $GO_VERSION
+
+# pyenv
+~/.anyenv/bin/anyenv install pyenv
+pyenv install $PYTHON_VERSION
+pyenv global $PYTHON_VERSION
+
+# rbenv
+~/.anyenv/bin/anyenv install rbenv
+rbenv install $RUBY_VERSION
+rbenv global $RUBY_VERSION
+
+# nodenv
+~/.anyenv/bin/anyenv install nodenv
+nodenv install $NODE_VERSION
+nodenv global $NODE_VERSION
+
+# Renv
+~/.anyenv/bin/anyenv install Rbenv
+Renv install $R_VERSION
+Renv global $R_VERSION
+
+# hsenv
+~/.anyenv/bin/anyenv install hsenv
+hsenv install $HASKELL_VERSION
+hsenv global $HASKELL_VERSION
+
+# tfenv
+~/.anyenv/bin/anyenv install tfenv
+tfenv install $TERRAFORM_VERSION
+tfenv global $TERRAFORM_VERSION
+
+# fzf
+git clone https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
